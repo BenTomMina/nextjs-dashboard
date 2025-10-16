6ebc3aca271c7bda8cc25d74cf048e8b34e5b3c9
Ben Mina <benjaminm@missionreadyhq.com>
2025-10-16 18:29:13 +1300

    Next change for Vercel issue

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,9 @@
 import NextAuth from "next-auth";
 import { authConfig } from "../../../../auth.config"; // adjust path if needed
 
-const authHandler = NextAuth(authConfig) as any;
+const authHandler = NextAuth(authConfig) as unknown as (
+  request: Request
+) => Response | Promise<Response>;
 
-export const GET = authHandler.GET;
-export const POST = authHandler.POST;
+export const GET = authHandler;
+export const POST = authHandler;
